2f89747ce0e937b5a0fda2e5e937c19126ceb8f8
oggsidedown <60523447+oggsidedown@users.noreply.github.com>
2020-01-31 22:28:31 +0000

    fixed to be normal

--- Monster.cpp
+++ Monster.cpp
@@ -14,19 +14,23 @@ public:
 		this->y = y;
 		this->direction = direction;
 	}
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		//movement
-		this->x += this->speed*this->direction;
-		this->y += this->gravity;
-		if(this->x<0){
-			this->x=0;
-			this->direction = 1;
-		}
-		if(this->x>1279){
-			this->x=1279;
-			this->direction = -1;
-		}
+	int gety(){
+		return this->y;
+	}
+	int getx(){
+		return this->x;
+	}
+	int getdirection(){
+		return this->direction;
+	}
+	int getspeed(){
+		return this->speed;
+	}
+	int getgravity(){
+		return this->gravity;
+	}
+	void sety(int yinput){
+		this->y=yinput;
 		if(this->y>=719){
 			this->y=719;
 			this->gravity=0;
@@ -37,13 +41,27 @@ public:
 		if(this->y<0){
 			this->y=0;
 		}
-		return true;
 	}
-	/*void Render()
-	{
-		// draws a pixel for the monster
-		Draw(this.x,this.y, olc::Pixel(100,100,100));
-	}*/
+	void setx(int xinput){
+		this->x=xinput;
+		if(this->x<0){
+				this->x=0;
+				this->direction = 1;
+		}
+		if(this->x>1279){
+				this->x=1279;
+				this->direction = -1;
+		}
+	}
+	void setspeed(int speedinput){
+		this->speed=speedinput;
+	}
+	void setdirection(int directioninput){
+		this->direction=directioninput;
+	}
+	void setgravity(int gravityinput){
+		this->gravity=gravityinput;
+	}
 private:
 	void readSpriteData(){
 	}
