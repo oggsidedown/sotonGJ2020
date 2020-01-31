a67d837f39c5ae6b23a3f149e5a7e53d5c3badde
MaundyMoo <maund007@gmail.com>
2020-01-31 23:22:52 +0000

    removing merge jargon

--- main.cpp
+++ main.cpp
@@ -15,34 +15,28 @@ public:
 	}
 	bool OnUserCreate() override
 	{
-<<<<<<< HEAD
 		// Called at the start	
 		for(int x = 0; x < ScreenWidth(); x++)
 			for(int y = 0; y < ScreenHeight(); y++)
 				Draw(x,y,olc::Pixel(50,200,50));
-=======
 		// Called at the start
 		int map[160][90]; //0 means nothing there, 1 means block there
 		int i, j;
 		for(i=0;i<160;i++)
 			for(j=0;j<90;j++)
 				map[i][j]=0;
->>>>>>> 184a417ec8802c1781d35a2c996d5b802a3c84d7
 		return true;
 	}
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		// Called once per frame
-<<<<<<< HEAD
 		
-=======
 		int closestTileX, closestTileY;
 		for(int x = 0; x < ScreenWidth(); x++)
 			for(int y = 0; y < ScreenHeight(); y++)
 				Draw(x,y,olc::Pixel(255,0,0));
 		Draw(this->player.getX(), this->player.getY(), olc::Pixel(100,100,100));
 		Draw(monster.getx(), monster.gety(), olc::Pixel(0,100,0);
->>>>>>> 184a417ec8802c1781d35a2c996d5b802a3c84d7
 		// Movement Logic	
 		if(GetKey(olc::Key::LEFT).bHeld)
 		{
@@ -61,10 +55,8 @@ public:
 			player.setY(SCREEN_HEIGHT - 32);
 		}
 
-<<<<<<< HEAD
 		// Rendering	
 		DrawSprite(this->player.getX(), this->player.getY(), new olc::Sprite("charTEMP.png"));
-=======
 		if(GetMouse(0)){
 			closestTileX = getMouseX()/8;
 			closestTileY = getMouseY()/8;
@@ -75,7 +67,6 @@ public:
 		// monster movement logic
 		monster.setx(monster.getx()+monster.getspeed()*monster.getdirection());
 		monster.sety(monster.gety + monster.getgravity);
->>>>>>> 184a417ec8802c1781d35a2c996d5b802a3c84d7
 		return true;
 	}
 	bool OnUserDestroy() override
